86e7164e5d2e2d5f15fff4e3872c739e59c039c4
yatoidomeojisan <142865623+yatoidomeojisan@users.noreply.github.com>
2023-08-24 07:52:20 +0900

    updte bio

--- src/components/bio.js
+++ src/components/bio.js
@@ -9,7 +9,7 @@ const Bio = () => {
             <div className="">
                 <StaticImage src="../images/me.jpg" alt="me" imgClassName="rounded-full" className="inline-block mb-4 mr-4 float-left"/>
                 <p className="">雇止め間近のアラフォーエンジニア。新天地を求めて予備試験に挑戦中。R6予備試験合格を目指しています。工学部卒で法律の「ほ」も知らないところからのスタート。R2から勉強開始して、R4、R5と二度受験して玉砕中。R6で三度目の正直となるか。</p>
-                <p>詳細な自己紹介は<Link to="/blog/introduction" className="underline">こちら</Link>。</p>
+                <p>詳細は<Link to="/blog/introduction" className="underline">こちら</Link>。</p>
             </div>
             <div className="clear-both"></div>
         </div>
